8e723f0236f042ce841944e1851177dd1a29c06b
mviniss <m_vinicius.ss@icloud.com>
2022-03-16 18:04:17 -0300

    Reposicionamento do book e adição da table
    A table foi adicionada e o book foi reposicionado

--- AlquimistaViajante/View/GameSceneLaboratory.swift
+++ AlquimistaViajante/View/GameSceneLaboratory.swift
@@ -14,5 +14,12 @@ class GameSceneLaboratory: SKScene {
         self.addChild(bookNode)
         bookNode.size = CGSize(width: 152.0, height: 152.0)
         bookNode.position = CGPoint(x: 165.45, y: 473.56)
+        bookNode.zPosition = 0
+        
+        let tableNode = SKSpriteNode(imageNamed: "table")
+        self.addChild(tableNode)
+        tableNode.size = CGSize(width: 1000, height: 557)
+        tableNode.position = CGPoint(x: 550, y: 367.19)
+        bookNode.zPosition = 1
     }
 }
