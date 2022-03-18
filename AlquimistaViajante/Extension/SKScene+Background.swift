82f64ceca848d8d369033e1b77734dc37520918d
Anna-andrade <annaandrade@id.uff.br>
2022-03-18 17:16:22 -0300

    criação do background floor

--- AlquimistaViajante/Extension/SKScene+Background.swift
+++ AlquimistaViajante/Extension/SKScene+Background.swift
@@ -29,17 +29,21 @@ extension SKScene {
             
             for i in 0...Int(width/side)+1 {
                 for j in 0...Int(height/side)+1{
-                let backgroundWallNode = SKSpriteNode(imageNamed: "Background")
-                backgroundWallNode.size = CGSize(width: side, height: side)
-                addChild(backgroundWallNode)
-                backgroundWallNode.position = CGPoint (x: backgroundWallNode.size.width*CGFloat(i), y: backgroundWallNode.size.height*CGFloat(j))
-                backgroundWallNode.zPosition = -1
+                    let backgroundWallNode = SKSpriteNode(imageNamed: "Background")
+                    backgroundWallNode.size = CGSize(width: side, height: side)
+                    addChild(backgroundWallNode)
+                    backgroundWallNode.position = CGPoint (x: backgroundWallNode.size.width*CGFloat(i), y: backgroundWallNode.size.height*CGFloat(j))
+                    backgroundWallNode.zPosition = -1
                     
-                let backgroundFloorNode = SKSpriteNode(imageNamed: "floor")
+                    let backgroundFloorNode = SKSpriteNode(imageNamed: "floor")
                     backgroundFloorNode.size = CGSize(width: side, height: side/2)
+                    addChild(backgroundFloorNode)
+                    backgroundFloorNode.position = CGPoint (x: backgroundFloorNode.size.width*CGFloat(i), y: backgroundWallNode.size.height*CGFloat(j))
+                    backgroundFloorNode.zPosition = 0
+                }
             }
+            
         }
         
     }
-    
 }
