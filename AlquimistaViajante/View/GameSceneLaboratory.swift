323c2b299569b98f17548db32aeed19b2b39242b
mviniss <m_vinicius.ss@icloud.com>
2022-03-18 16:09:48 -0300

    Atualização da GameSceneLaboratory
    Marcus reposicionou alguns elementos da GameSceneLaboratory

--- AlquimistaViajante/View/GameSceneLaboratory.swift
+++ AlquimistaViajante/View/GameSceneLaboratory.swift
@@ -25,7 +25,7 @@ class GameSceneLaboratory: SKScene {
         
         let tableNode = SKSpriteNode(imageNamed: "table")
         self.addChild(tableNode)
-        tableNode.size = CGSize(width: 950, height: 400)
+        tableNode.size = CGSize(width: 1045, height: 440)
         tableNode.position = CGPoint(x: 550, y: 200)
         bookNode.zPosition = 4
         
@@ -37,46 +37,46 @@ class GameSceneLaboratory: SKScene {
         
         let bookcaseNode = SKSpriteNode(imageNamed: "bookcase")
         self.addChild(bookcaseNode)
-        bookcaseNode.size = CGSize(width: 170, height: 150)
+        bookcaseNode.size = CGSize(width: 204, height: 180)
         bookcaseNode.position = CGPoint(x: 250, y: 200)
         bookcaseNode.zPosition = 2
         
         let shelfNode = SKSpriteNode(imageNamed: "shelf")
         self.addChild(shelfNode)
         shelfNode.size = CGSize(width: 950, height: 150)
-        shelfNode.position = CGPoint(x: 550, y: 500)
+        shelfNode.position = CGPoint(x: 550, y: 600)
         shelfNode.zPosition = 1
         
         let bunsenBurnerNode = SKSpriteNode(imageNamed: "bunsenBurner")
         self.addChild(bunsenBurnerNode)
         bunsenBurnerNode.size = CGSize(width: 150, height: 150)
-        bunsenBurnerNode.position = CGPoint(x: 250, y: 620)
+        bunsenBurnerNode.position = CGPoint(x: 250, y: 720)
         
         let glassFunnelNode = SKSpriteNode(imageNamed: "glassFunnel")
         self.addChild(glassFunnelNode)
         glassFunnelNode.size = CGSize(width: 150, height: 150)
-        glassFunnelNode.position = CGPoint(x: 460, y: 620)
+        glassFunnelNode.position = CGPoint(x: 460, y: 720)
         
         let flatBottomFlaskNode = SKSpriteNode(imageNamed: "flatBottomFlask")
         self.addChild(flatBottomFlaskNode)
         flatBottomFlaskNode.size = CGSize(width: 150, height: 150)
-        flatBottomFlaskNode.position = CGPoint(x: 650, y: 620)
+        flatBottomFlaskNode.position = CGPoint(x: 650, y: 720)
         
         let testTubeNode = SKSpriteNode(imageNamed: "testTube")
         self.addChild(testTubeNode)
         testTubeNode.size = CGSize(width: 150, height: 150)
-        testTubeNode.position = CGPoint(x: 850, y: 620)
+        testTubeNode.position = CGPoint(x: 850, y: 720)
         
         let mortarNode = SKSpriteNode(imageNamed: "mortar")
         self.addChild(mortarNode)
         mortarNode.size = CGSize(width: 120, height: 120)
-        mortarNode.position = CGPoint(x: 460, y: 170)
+        mortarNode.position = CGPoint(x: 460, y: 270)
         mortarNode.zPosition = 5
         
         let mortarBottomNode = SKSpriteNode(imageNamed: "mortarBottom")
         self.addChild(mortarBottomNode)
         mortarBottomNode.size = CGSize(width: 120, height: 120)
-        mortarBottomNode.position = CGPoint(x: 460, y: 170)
+        mortarBottomNode.position = CGPoint(x: 460, y: 270)
         mortarBottomNode.zPosition = 5
     }
 }
