3eb50566ef389c800252bd9973749bb3323cea85
Anna-andrade <annaandrade@id.uff.br>
2022-03-18 16:10:14 -0300

    Correção e arrumação Extension e GameScene
    Correção e arrumação Extension e GameScene

--- AlquimistaViajante/Extension/SKScene+Background.swift
+++ AlquimistaViajante/Extension/SKScene+Background.swift
@@ -13,9 +13,10 @@ extension SKScene {
         let width = self.size.width
         let height = self.size.height
         
-        for i in 0...Int(width/side) {
-            for j in 0...Int(height/side){
+        for i in 0...Int(width/side)+1 {
+            for j in 0...Int(height/side)+1{
                 let backgroundWallNode = SKSpriteNode(imageNamed: "Background")
+                backgroundWallNode.size = CGSize(width: side, height: side)
                 addChild(backgroundWallNode)
                 backgroundWallNode.position = CGPoint (x: backgroundWallNode.size.width*CGFloat(i), y: backgroundWallNode.size.height*CGFloat(j))
                 backgroundWallNode.zPosition = -1
