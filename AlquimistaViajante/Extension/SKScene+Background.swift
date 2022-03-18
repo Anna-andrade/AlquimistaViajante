6074f2dbac952eeaf1d987078853205ca1aaa3aa
Anna-andrade <annaandrade@id.uff.br>
2022-03-18 17:08:15 -0300

    Renomear assets para consertar erro
    - background e flask atualizados

--- AlquimistaViajante/Extension/SKScene+Background.swift
+++ AlquimistaViajante/Extension/SKScene+Background.swift
@@ -23,6 +23,22 @@ extension SKScene {
                 
             }
         }
+        func drawBackgroundFloor(side: CGFloat) {
+            let width = self.size.width
+            let height = self.size.height
+            
+            for i in 0...Int(width/side)+1 {
+                for j in 0...Int(height/side)+1{
+                let backgroundWallNode = SKSpriteNode(imageNamed: "Background")
+                backgroundWallNode.size = CGSize(width: side, height: side)
+                addChild(backgroundWallNode)
+                backgroundWallNode.position = CGPoint (x: backgroundWallNode.size.width*CGFloat(i), y: backgroundWallNode.size.height*CGFloat(j))
+                backgroundWallNode.zPosition = -1
+                    
+                let backgroundFloorNode = SKSpriteNode(imageNamed: "floor")
+                    backgroundFloorNode.size = CGSize(width: side, height: side/2)
+            }
+        }
         
     }
     
