3a30b013ca8bad30fef551caf4e33a5a5a1cf9f3
Anna-andrade <annaandrade@id.uff.br>
2022-03-21 17:52:00 -0300

    Ajustando proporção do tamanho do node
    Ajustando proporção do tamanho do Node para quando alterar a medida da tela

--- AlquimistaViajante/Controller/GameViewController.swift
+++ AlquimistaViajante/Controller/GameViewController.swift
@@ -14,7 +14,7 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         let skView = SKView()
-        let scene = GameSceneLaboratory()
+        let scene = GameSceneReaction()
         scene.scaleMode = .aspectFit
         scene.size = CGSize(width: self.view.frame.width, height: self.view.frame.width)
         skView.presentScene(scene)
